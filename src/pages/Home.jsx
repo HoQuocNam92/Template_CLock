afeadf86e9f06822d8543d2bd9ad30f1814211e6
hoquocnam92 <hoquocnam92@gmail.com>
2025-03-03 22:52:26 +0700

    DONE

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,12 +4,20 @@ const Home = () => {
   return (
     <div>
       <Header />
-      <div>
-        <img
-          className="h-[835px] max-w-[100%]  "
-          src="http://127.0.0.1:5500/images/slider-bg.jpg"
-          alt="Banner"
-        />
+      <div className="relative">
+        <img className=" w-full " src="/images/slider-bg.jpg" alt="Banner" />
+        <div className="absolute  top-[50%] left-[20%] transform   -translate-y-1/2">
+          <h1>STYLISH WATCHES</h1>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus
+            quidem maiores perspiciatis, illo maxime voluptatem a itaque
+            suscipit.
+          </p>
+          <div>
+            <button>Contact Us</button>
+            <button>About Us</button>
+          </div>
+        </div>
       </div>
     </div>
   );
